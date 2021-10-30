00ca73119b8f438b548837189466348cc877fb22
Georgios Giasemidis <g.giasemidis@gmail.com>
2021-10-30 18:04:04 +0100

    first commit

---
+++ utils/shot_chart_plots.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+from draw_court import draw_court
+
+
+def plot_scatter(made, miss, title=None):
+    """
+    Scatter plot of made and missed shots
+    """
+    plt.figure()
+    draw_court()
+    plt.plot(made['COORD_X'], made['COORD_Y'], 'o', label='Made')
+    plt.plot(miss['COORD_X'], miss['COORD_Y'], 'x', markerfacecolor='none',
+             label='Missed')
+    plt.legend()
+    plt.xlim([-800, 800])
+    plt.ylim([-200, 1300])
+    plt.title(title)
+    plt.show()
+    return
+
+
+def joint_plot(df, kind='hex', title=None):
+    """
+    Density plot of shots as joint distributions of x and y coordinates
+    """
+    cmap = plt.cm.gist_heat_r
+    joint_shot_chart = sns.jointplot(x=df['COORD_X'], y=df['COORD_Y'],
+                                     kind='hex', space=0, color=cmap(.2),
+                                     cmap=cmap, joint_kws={"gridsize": 15})
+
+    # A joint plot has 3 Axes, the first one called ax_joint
+    # is the one we want to draw our court onto
+    ax = joint_shot_chart.ax_joint
+    draw_court(ax)
+    plt.xlim([-800, 800])
+    plt.ylim([-200, 1300])
+    plt.title(title)
+    plt.show()
+    return
